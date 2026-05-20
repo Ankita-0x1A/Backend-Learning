b2cb80139538050a0e93671a809634b0e9b3e901
CodingButterfly-0X1A <sah.ankita213@gmail.com>
2026-05-20 20:20:34 +0530

    Api response and error handling

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+const app = express();
+// import express from "express";
 
 dotenv.config({
     path: "./env"
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server running on port ${process.env.PORT}`)
+    })
+
+    app.on("error",(error)=>{
+        console.log("mongodb connection failed");
+        throw error
+    })
+})
+.catch((error)=>{
+    console.log("mongodb connection failed !!!! ",error);
+})
 
 
 
